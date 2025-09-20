10b43528d7a2f8fb64a0d78a2d77925a824325cb
hridaylakhani05-hue <hridaylakhani05@gmail.com>
2025-09-20 20:18:53 +0530

    updates

--- routes/alumniRouter.js
+++ routes/alumniRouter.js
@@ -7,12 +7,12 @@ const alumniController=require("../Controllers/alumniController")
 
 const postJobController = require("../controllers/postJobController");
 
-alumniRouter.post("/:userId/post-job", alumniController.postJob);
+alumniRouter.post("/:userId/post-job", postJobController.postJob);
 
-alumniRouter.get("/:userId/post-job", alumniController.getPostedJobs);
+alumniRouter.get("/:userId/post-job", postJobController.getPostedJobs);
 
-alumniRouter.put("/:userId/post-job/:jobId", alumniController.updatePostedJob);
+alumniRouter.put("/:userId/post-job/:jobId", postJobController.updatePostedJob);
 
-alumniRouter.delete("/:userId/post-job/:jobId", alumniController.deletePostedJob);
+alumniRouter.delete("/:userId/post-job/:jobId", postJobController.deletePostedJob);
 
 module.exports = alumniRouter;
